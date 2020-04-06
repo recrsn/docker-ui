d9a611cb2d059937cfed2d0af197f45e7e43ea0b
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2020-04-07 03:20:02 +0530

    Add a sidebar

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var Docker = require('dockerode');
-var docker = new Docker({socketPath: '/var/run/docker.sock'});
-var table = document.getElementById("docker-ps")
+const Docker = require('dockerode');
+
+const docker = new Docker();
+
+const table = document.getElementById("docker-ps")
+
 docker.listContainers(function (err, containers) {
-    containers.forEach(function (containerInfo) {
-      var row = table.insertRow();
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      cell1.innerHTML = containerInfo.Id.substring(0,12);
-      cell2.innerHTML = containerInfo.Names[0];
-      cell3.innerHTML = containerInfo.Image;
-    });
+  containers.forEach(function (containerInfo) {
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    cell1.innerHTML = containerInfo.Id.substring(0, 12);
+    cell2.innerHTML = containerInfo.Names[0];
+    cell3.innerHTML = containerInfo.Image;
   });
+});
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hiddenInset',
+    vibrancy: 'sidebar',
     webPreferences: {
       nodeIntegration: true
     }
@@ -12,9 +14,6 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
